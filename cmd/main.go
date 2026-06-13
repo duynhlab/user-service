2a7fdd8167591fb1be2d726676b4f983e143148f
Duynh ne <duyne250897@gmail.com>
2026-06-13 23:24:45 +0700

    Adopt pkg v0.5.0 error envelope and zap logger

--- cmd/main.go
+++ cmd/main.go
@@ -16,6 +16,7 @@ import (
 
 	"github.com/duynhlab/pkg/authmw"
 	"github.com/duynhlab/pkg/grpcx"
+	"github.com/duynhlab/pkg/logger/zapx"
 	"github.com/duynhlab/pkg/migratex"
 	"github.com/duynhlab/pkg/obsx"
 	authv1 "github.com/duynhlab/pkg/proto/auth/v1"
@@ -31,7 +32,7 @@ import (
 func main() {
 	cfg := config.Load()
 
-	logger, err := middleware.NewLogger()
+	logger, err := zapx.New(os.Getenv("LOG_LEVEL"))
 	if err != nil {
 		panic("Failed to initialize logger: " + err.Error())
 	}
